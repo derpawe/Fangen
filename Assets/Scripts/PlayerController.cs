1c04ea046853f88d98ffc0f94db988f8c6d56d47
Patrick Weiner <patrick.weiner@pawe.at>
2015-07-04 13:17:47 +0200

    new PlayerController and CameraFollow functions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start ()
+	{
+        // Set Camera to new player
+	    mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	    mainCamera.GetComponent<CameraFollow>().SetCameraToTarget(this.transform);
+	}
+}
