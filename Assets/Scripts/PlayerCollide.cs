2d4e4133cc021cc159cefa5ea68aebaab595f4d9
Peter Kohout <Peter_kohout@gmx.at>
2015-07-05 11:38:23 +0200

    com

--- Assets/Scripts/PlayerCollide.cs
+++ Assets/Scripts/PlayerCollide.cs
@@ -1,28 +1,35 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
 
 public class PlayerCollide : MonoBehaviour {
-
+	
 	float _timer;
 	bool _timeout;
+	[SyncVar]
+	int points;
+
+	[SyncVar]
 	bool _hunter;
+
 	Color red;
 	Color blue;
+	bool isFirstplayer;
 	NetworkIdentity nIdent;
 	MeshRenderer gameObjectRenderer;
 	// Use this for initialization
 	void Start () {
+		points = 0;
 		_timer = 2;
 		_timeout = false;
 		nIdent = GetComponent<NetworkIdentity> ();
 		_hunter = (nIdent.netId.Value == 2);
-		
+		isFirstplayer = !_hunter;
 		red = new Color(255,0,0,1);
 		blue = new Color(0,0,255,1);
-
+		
 		gameObjectRenderer = this.gameObject.GetComponent<MeshRenderer>();
-
+		
 		Material newMaterial = new Material(Shader.Find("Diffuse"));
 		if (_hunter)
 			newMaterial.color = red;
@@ -31,17 +38,27 @@ public class PlayerCollide : MonoBehaviour {
 		gameObjectRenderer.material = newMaterial ;
 	}
 	
+
+	void OnGUI(){
+		if (isFirstplayer)
+			GUI.TextArea (new Rect (10, 10,100, 25), "Erster:" + points);
+		else
+			GUI.TextArea (new Rect (10, 10, 100, 25), "Zweiter:" + points);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (_timeout) {
 			_timer -= Time.deltaTime;
-			if (_timer <= 0)
+			if (_timer <= 0){
 				_timer = 2;
 				_timeout = false;
+			}
 		}
-
+		if(_hunter)
+			points++;
 	}
-
+	
 	void OnCollisionEnter(Collision col) {
 		print ("not if");
 		if(col.gameObject.tag == "Player"){
@@ -50,7 +67,7 @@ public class PlayerCollide : MonoBehaviour {
 				_hunter = false;
 				_timeout = true;
 				print ("isHunter");
-
+				
 				Material newMaterial = new Material(Shader.Find("Diffuse"));
 				
 				newMaterial.color = blue;
@@ -59,14 +76,13 @@ public class PlayerCollide : MonoBehaviour {
 				_hunter = true;
 				_timeout = true;
 				print("isHunted");
-
+				
 				Material newMaterial = new Material(Shader.Find("Diffuse"));
 				
 				newMaterial.color = red;
 				gameObjectRenderer.material = newMaterial ;
 			} 
-
+			
 		}
 	}
 }
-
