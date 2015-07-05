e579b6c6405f59b113ef0df15f2ca4deb105a3ae
Peter Kohout <Peter_kohout@gmx.at>
2015-07-05 12:38:25 +0200

    str

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -7,8 +7,11 @@ public class LightSwitch : NetworkBehaviour
 {
 
     public bool switchable = true;
+
     [SyncVar]
 	public bool isOn = true;
+
+	public bool localOn = true;
 
     public AudioClip lightOnClip;
     public AudioClip lightOffClip;
@@ -35,6 +38,16 @@ public class LightSwitch : NetworkBehaviour
         SwitchLight();
     }
 
+	void Update()
+	{
+		if (localOn != isOn) 
+		{
+			SwitchLight();
+			localOn = isOn;
+		}
+		print(isOn);
+	}
+
     private void SwitchLight()
     {
         if (switchable && isOn)
@@ -42,12 +55,14 @@ public class LightSwitch : NetworkBehaviour
             AudioSource.PlayClipAtPoint(lightOffClip, transform.position, 1f);
             light.range = 1;
             isOn = false;
+			localOn = false;
         }
         else if(switchable && !isOn)
         {
             AudioSource.PlayClipAtPoint(lightOnClip, transform.position, 1f);
             light.range = 10;
             isOn = true;
+			localOn = true;
         }
     }
 }
