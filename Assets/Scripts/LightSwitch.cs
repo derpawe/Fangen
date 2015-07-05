91463586cf67be656f8ddf388a7857f8e6052434
Patrick Weiner <patrick.weiner@pawe.at>
2015-07-05 09:23:57 +0200

    lightswitch

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour
+{
+
+    public bool switchable = true;
+
+    public AudioClip lightOnClip;
+    public AudioClip lightOffClip;
+
+    private Light light;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    light = GetComponentInChildren<Light>();
+	}
+
+    void OnMouseDown()
+    {
+        SwitchLight();
+    }
+
+    private void SwitchLight()
+    {
+        if (switchable && light.enabled == true)
+        {
+            AudioSource.PlayClipAtPoint(lightOffClip, transform.position, 1f);
+            light.enabled = false;
+        }
+        else if(switchable && light.enabled == false)
+        {
+            AudioSource.PlayClipAtPoint(lightOnClip, transform.position, 1f);
+            light.enabled = true;
+        }
+    }
+}
