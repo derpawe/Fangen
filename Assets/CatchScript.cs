d7f4c7d29e83c9a3c04bf065fc9e8c806f2ebefa
Johanna Pirker <joeyprink@gmail.com>
2015-07-05 15:21:55 +0200

    a

--- Assets/CatchScript.cs
+++ Assets/CatchScript.cs
@@ -6,12 +6,14 @@ public class CatchScript : MonoBehaviour {
 
 	float _timer;
 	bool _timeout;
-	public bool isHunter = true;
+	public bool isHunter;
 	int points = 0;
+	public string huntingString = "";
+
 
 	// Use this for initialization
 	void Start () {
-	
+	//	print (gameObject.name);
 	}
 	
 	void Update () {
@@ -24,31 +26,51 @@ public class CatchScript : MonoBehaviour {
 		}
 		if(!isHunter)
 			points++;
+	
 	}
 
 
 	void OnGUI(){
 		if (isHunter)
-			GUI.TextArea (new Rect (10, 10,100, 25), "Catch!  " + points);
+			GUI.TextArea (new Rect (30, 10,100, 25), huntingString + points);
 		else
-			GUI.TextArea (new Rect (500, 10, 100, 25), "Run!  "+ points);
+			GUI.TextArea (new Rect (700, 10, 100, 25), huntingString + points);
 	}
 
 	
 
 	void OnTriggerEnter(Collider collider){
-		if(collider.tag  == "Player"){
-			print("Collision ");
+		if(collider.name  == "Madel" && gameObject.name == "Bub"){
+		//	print("Collision Madel und Bub ");
 			if(isHunter) {
-				points = points + 10000;
+				//points = points + 10000;
 				isHunter = false;
+				huntingString = "Run!!! ";
 				//_timeout = true;
 			} else {
 				isHunter = true;
 				//_timeout = true;
-				print("DAMN " + collider.name);
+			//	print("DAMN " + collider.name);
+				//points = points - 10000;
 			} 
-			
+
+			transform.position = new Vector3 (-6.11f, 0f, -9.2f);
+		}
+		else if(collider.name  == "Bub" && gameObject.name == "Madel"){
+		//	print("Collision Madel und Bub ");
+			if(isHunter) {
+				//points = points + 10000;
+				isHunter = false;
+				huntingString = "Catch!!! ";
+				//_timeout = true;
+			} else {
+				isHunter = true;
+				//_timeout = true;
+			//	print("DAMN " + collider.name);
+				//points = points - 10000;
+			} 
+
+			transform.position = new Vector3 (-3.01f, 0f, -12.3f);
 		}
 	}
 }
