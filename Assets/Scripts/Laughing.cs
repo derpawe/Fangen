c8c63c35a6026474597a77563f6f09d92ad37e56
Patrick Weiner <patrick.weiner@pawe.at>
2015-07-05 13:20:57 +0200

    laughing for the kids

---
+++ Assets/Scripts/Laughing.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laughing : MonoBehaviour
+{
+
+    public AudioClip[] audioClips;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Laugh());
+	}
+	
+
+    public IEnumerator Laugh()
+    {
+        int clipToPlay = Random.Range(0, audioClips.Length - 1);
+
+        Debug.Log(clipToPlay);
+
+        AudioClip audioClip = audioClips[clipToPlay];
+
+        AudioSource.PlayClipAtPoint(audioClip, transform.position, 1f);
+
+        yield return new WaitForSeconds(5f);
+
+
+        StartCoroutine(Laugh());
+
+        yield return 0;
+
+    }
+}
